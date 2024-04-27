7823ea0c41f0ab0aa28728855dae776a3af89559
Khadim <khadimfall0602@gmail.com>
2024-04-27 15:43:02 +0200

    :)

--- app/neo4j_utils.py
+++ app/neo4j_utils.py
@@ -8,14 +8,13 @@ class Subject:
         self.isControl = disease == "control"
 
     def __repr__(self):
-        return f"Subject(subjectId={self.subjectId}, disease={self.disease}, icd10={self.icd10} hasIcd10={self.hasIcd10} icdFirstLetter={self.icdFirstLetter} isControl={self.isControl})"
-        # return f"Subject(subjectId={self.subjectId}, disease={self.disease}, icd10={self.icd10})"
+        return f"Subject(subjectId={self.subjectId}, disease={self.disease}, icd10={self.icd10})"
 
 
 def get_subjects(session):
     node_count_query = """MATCH (b:Biological_sample)-->(d:Disease)
 WITH *, [s in d.synonyms WHERE s STARTS WITH "ICD10CM" | s] as ICD10
-RETURN b.subjectid as subjectId, d.name as disease, ICD10[0] as icd10"""     
+RETURN b.subjectid as subjectId, d.name as disease, ICD10[0] as icd10 LIMIT 10"""     
     data = session.run(node_count_query).data()
     subjects = [Subject(**record) for record in data]
     return subjects
